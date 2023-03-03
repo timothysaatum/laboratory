ba28b6372f45ef9a65be2227b0f93925c8e4f28c
timothysaatum <tsaatum@outlook.com>
2023-03-03 05:35:45 +0000

    Added two cards

--- LDMSApp/static/LDMSApp/js/style.js
+++ LDMSApp/static/LDMSApp/js/style.js
@@ -1,5 +1,7 @@
-// Smooth scroll to top
-$('footer a[href="#top"]').on('click', function(){
-  $('html, body').animate({scrollTop:0}, 'slow');
-  return false;
-});
+$(document).ready(function(){
+  //smoot scroll to the top
+  $('footer a[href="#top"]').on('click', function(){
+    $('html, body').animate({scrollTop:0}, 'slow');
+    return false;
+  })
+}); 
